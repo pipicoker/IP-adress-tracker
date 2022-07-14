71427a29f98f33ca454b023cadaf87c4198cba3a
pipicoker <89666696+pipicoker@users.noreply.github.com>
2022-07-14 06:24:15 +0100

    changed the directory of the background image in the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        'pattern-background' : "url(/public/images/pattern-bg.png)"
+        'pattern-background' : "url(images/pattern-bg.png)"
       }
     },
   },
